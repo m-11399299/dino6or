c01f6f07da2f9eb85d1fe8731d9d9ee529541500
m-11399299 <115517968+m-11399299@users.noreply.github.com>
2022-10-18 11:19:12 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
 basic.forever(function () {
-	
+    if (true) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() < 100) {
+        pins.servoWritePin(AnalogPin.P2, 100)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 0)
+    }
 })
